d6bd993b3d8832d6b4cea34838b1b88d86212a79
Gaurav Patel <108782700+gauravpatel222@users.noreply.github.com>
2025-01-23 11:15:04 +0530

    Delete bridegelab.java

--- bridegelab.java
+++
@@ -1,7 +0,0 @@
-package Assignment_01_19thJanuary;
-
-public class WelcomeToBridgeLabz {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
