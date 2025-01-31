cea9649b57ed1e999e726b4f73c17ee7a596c29e
Gaurav <gauravgurjar8791@gmail.com>
2025-01-31 11:48:45 +0530

    making changes

--- Book.java
+++ Book.java
@@ -2,13 +2,18 @@ public class Book {
     private String title;
     private String author;
     private int price;
-    public void display(String title,String author,int price){
+    public Book(String title,String author,int price){
+        this.author=author;
+        this.price=price;
+        this.title=title;
+    }
+    public void display(){
         System.out.println("titlev "+title);
         System.out.println("author "+author);
         System.out.println("price "+price);
     }
     public static void main(String[] args) {
-        Book b=new Book();
-        b.display("Harray Potter","Gaurav Guzzar",1000000);
+        Book b=new Book("Harray Potter","Gaurav Guzzar",1000000);
+        b.display();
     }
 }
--- circle.java
+++ circle.java
@@ -1,14 +1,41 @@
-public class circle {
+// Circle.java
+import java.util.Scanner;
+
+public class Circle {
+   
     private double radius;
-    public double area(double radius){
-                return Math.PI*Math.pow(radius,2);
+
+   
+    public Circle(double radius) {
+        this.radius = radius;
     }
-    public double circumference(double radius){
-        return 2*Math.PI*radius;
+
+   
+    public double getArea() {
+        return Math.PI * radius * radius;
     }
+
+    
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+ 
+    public void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Circumference: " + getCircumference());
+    }
+
+    
     public static void main(String[] args) {
-        circle c = new circle();
-        System.out.println(c.area(23));
-        System.out.println(c.circumference(2));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
+
+        Circle c = new Circle(r);
+        c.displayDetails();
+
+        sc.close();
     }
 }
